50132c2c155cd31b6eb3a133773cb667f3bdd4d6
bestow-wings <dylanjsw@outlook.com>
2021-12-05 15:44:10 +1000

    Got the links working woo

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cards from "./components/Cards"
 
+import chess_ai_gif from "./img/chess_ai.gif"
+
 const projects = [
   {
     id: 1, 
-    title: "Project 1",
-    description: "A project"
+    title: "Chess AI",
+    description: "Playable Chess AI with TensorFlow",
+    imgsrc: chess_ai_gif,
+    link: "https://medium.com/@dylanjsw/playable-chess-ai-with-tensorflow-26c318207452"
   },
   {
     id: 2,
     title: "Project 2",
-    description: "A project"
+    description: "A project",
+    imgsrc:""
   },
   {
     id: 3, 
     title: "Project 3",
-    description: "A project"
+    description: "A project",
+    imgsrc:""
   },
   {
     id: 4, 
     title: "Project 4",
-    description: "A project"
+    description: "A project",
+    imgsrc:""
   }
 ]
 
@@ -61,18 +68,15 @@ function App() {
         </div>
         <p></p>
         <Cards data ={services} />
-        <div style={{textAlign: "center"}}>
-          <p>Connect.</p>
-          <div style={{backgroundColor:"white", color:"black", justifyContent:"space-evenly", display:"flex"}}>
-            <div style={{padding: "0.5em"}}>
-              Medium
-            </div>
-            <div style={{padding: "0.5em"}}>
-              GitHub
-            </div>
-            <div style={{padding: "0.5em"}}>
-              Email
-            </div>
+        <div style={{textAlign: "center", justifyContent:"space-evenly", display:"flex"}}>
+          <div style={{padding: "0.5em"}}>
+            Medium
+          </div>
+          <div style={{padding: "0.5em"}}>
+            GitHub
+          </div>
+          <div style={{padding: "0.5em"}}>
+            Email
           </div>
         </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import "../index.css"
+import chess_ai_image from "../img/chess_ai.gif"
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, imgsrc, link }) => {
     return (
-        <div className="card">
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </div>
+        <a href={link} target="_blank" className="card-image" style={{backgroundImage: `url("${imgsrc}")`}}>
+            <div className="card-background" >
+                <div className="card-text">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </a>
     )
 }
 
 export default Card
-    
