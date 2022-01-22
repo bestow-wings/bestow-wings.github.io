c2eabb4134ab66b118c379266081fc20bd6b0cbf
bestow-wings <dylanjsw@outlook.com>
2022-01-22 20:28:15 +1000

    Made it so the page dynamically updates with the prediction...
    next step is to get user inputs and use those to make the prediction

--- src/TitanicSurvivor.js
+++ src/TitanicSurvivor.js
@@ -1,21 +1,48 @@
-import { model } from "@tensorflow/tfjs";
+import { useState } from 'react';
+
 import "./index.css"
 
 const tf = require("@tensorflow/tfjs")
 
-async function generateModel() {
+function findLargestValueIndex(someArray) {
+  var ret = 0;
+  for (let i = 1; i < someArray.length; i++) {
+    if (someArray[i] < someArray[ret]) {
+      ret = i;
+    }
+  }
+  return ret;
+}
+
+function parsePreds (index) {
+  const survived =  index && 0;
+  if (survived) { return "Yes" }
+  else { return "No" }
+}
+
+async function loadModel() {
   // Demonstrate that the loaded model also works
   const loadedModel = await tf.loadLayersModel('saved_model/model.json');
   loadedModel.compile({
     optimizer: tf.train.adam(),
     loss: 'sparseCategoricalCrossentropy',
-    metrics: ['accuracy']
+    metrics: ['accuracy'] 
   })
-  console.log("Compiled.");
+
+  return loadedModel;
+}
+
+async function predictSurvival(model, stateMethod, args) {
+  var pred = await model.predict(tf.tensor([[3, 2, -1  , 0, 0, 7.2291999  , 1]])).dataSync();
+  stateMethod(parsePreds(findLargestValueIndex(pred)));  
 }
 
 function TitanicSurvivor() {
-  generateModel();
+
+  const [survived, setSurvived] = useState("Waiting");
+
+  loadModel().then((model) => predictSurvival(model, setSurvived, {}));
+
   return (
     <div className = "titanic-survivor">
       <div className = "titanic-survivor-heading">
@@ -31,6 +58,7 @@ function TitanicSurvivor() {
           <li>Parch</li>
           <li>Embarked</li>
         </ul>
+        <p>{survived}</p>
       </div>
     </div>
   );
